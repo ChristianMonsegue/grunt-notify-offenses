6b5c1de7e576aa2369f3dd12988503a7a4da368e
Christian Monsegue <christian.monsegue@kinnser.com>
2013-11-01 18:18:48 -0500

    Removals and Data Type Splits
    Removed some leftover code and split the return type into an object
    containing both output for file and stout.

--- tasks/notify_inline_css.js
+++ tasks/notify_inline_css.js
@@ -21,44 +21,47 @@ module.exports = function( grunt ) {
       separator: ', '
     });
 
-    function findOffenses ( src ) {
+    function defaultFindOffenses ( src ) {
       var lines = src.split(linefeed),
-      out = '',
-      column = 0,
-      column_offset = 0,
-      offending_css = '',
-      formatted_line = '',
-      is_offending = false,
+      file_out = "",
+      stout_out = "",
+      column,
+      column_offset,
+      offending_css,
+      formatted_line,
       hr = '_______________________________________' + linefeed;
 
       for(var i = 0; i < lines.length; i++){
-        offending_css = ''; column = 0; column_offset = 0;
+        offending_css = ""; column = 0; column_offset = 0;
         if(lines[i].length > 0){
           var sliced_line = lines[i].slice(column);
           while (offending_css !== null) {
-            offending_css = sliced_line.match(/<[\S\s]*style\s*=\s*(\"|\')[\S\s]*(\"|\')[\S\s]*>?[\S\s]*<?[\s\S]*>/i);
-            column = sliced_line.search(/<[\S\s]*style\s*=\s*(\"|\')[\S\s]*(\"|\')[\S\s]*>?[\S\s]*<?[\s\S]*>/i);
+            offending_css = sliced_line.match(/style\s*=\s*(\"|\')[\S\s]*(\"|\')[\S\s]*>?[\S\s]*<?[\s\S]*>/i);
+            column = sliced_line.search(/style\s*=\s*(\"|\')[\S\s]*(\"|\')[\S\s]*>?[\S\s]*<?[\s\S]*>/i);
             if(offending_css !== null) {
-              //is_offending = true;
-              out = out + ("->").yellow.bold + "Style attribute located at: " + ("L" + (i + 1)).bold.white + (" C" + (column + column_offset + 1)).bold.white + "." + linefeed;
+              file_out = file_out + ("->").yellow.bold + "Style attribute located at: " + ("L" + (i + 1)).bold.white + (" C" + (column + column_offset + 1)).bold.white + "." + linefeed;
+              stout_out = stout_out + "->" + "Style attribute located at: " + "L" + (i + 1) + " C" + (column + column_offset + 1) + "." + linefeed;
             }
             sliced_line = lines[i].slice(column + column_offset + 1);
             column_offset = column_offset + column + 1;
           }
         }
-        if(column_offset > 0) { out = out + hr + ("Offending line: ").green.bold  + lines[i] + linefeed + linefeed; }
+        if(column_offset > 0) {
+          file_out = file_out + hr + ("Offending line: ").green.bold  + lines[i] + linefeed + linefeed;
+          stout_out = stout_out + hr + "Offending line: " + lines[i] + linefeed + linefeed;
+        }
       }
-      return out;
+      return { file_out: file_out, stout_out: stout_out };
     }
 
     function outputLog ( src, dest, data ) {
       var output_file = '';
       grunt.log.write(linefeed);
       for(var k = 0; k < data.length; k++) {
-        var header = "Checking for inline css style in file: " + src[k] + linefeed;
-        output_file = output_file + header + data[k] + linefeed;
+        var header = "[Checking for inline css style in file: " + src[k] + "]" + linefeed;
+        output_file = output_file + header + data[k]['stout_out'] + linefeed;
         header = header.red.bold;
-        grunt.log.write(header + data[k] + linefeed);
+        grunt.log.write(header + data[k]['file_out'] + linefeed);
       }
       grunt.file.write(dest.dest, output_file);
       var creation_notify = 'File "' + dest.dest + '" created.';
@@ -69,7 +72,7 @@ module.exports = function( grunt ) {
       var paths = [];
       var src = f.src.filter( function ( filepath ) {
           if (!grunt.file.exists(filepath)) {
-            grunt.log.warn('Source file "' + filepath + '" not found.');
+            grunt.file.write('Source file "' + filepath + '" not found.');
             return false;
           } else {
             return true;
@@ -80,36 +83,7 @@ module.exports = function( grunt ) {
         });
         var all_out = [];
         for(var j = 0; j < src.length; j++){
-          all_out.push(findOffenses(src[j]));
-          /*var lines = src[j].split(linefeed),
-          out = '',
-          column = 0,
-          column_offset = 0,
-          offending_css = '',
-          formatted_line = '',
-          is_offending = false,
-          hr = '_______________________________________' + linefeed;
-
-          for(var i = 0; i < lines.length; i++){
-            offending_css = ''; column = 0; column_offset = 0;
-            if(lines[i].length > 0){
-              var sliced_line = lines[i].slice(column);
-              while (offending_css !== null) {
-                offending_css = sliced_line.match(/<[\S\s]*style\s*=\s*(\"|\')[\S\s]*(\"|\')[\S\s]*>?[\S\s]*<?[\s\S]*>/i);
-                column = sliced_line.search(/<[\S\s]*style\s*=\s*(\"|\')[\S\s]*(\"|\')[\S\s]*>?[\S\s]*<?[\s\S]*>/i);
-                if(offending_css !== null) {
-                  //is_offending = true;
-                  out = out + ("->").yellow.bold + "Style attribute located at: " + ("L" + (i + 1)).bold.white + (" C" + (column + column_offset + 1)).bold.white + "." + linefeed;
-                }
-                sliced_line = lines[i].slice(column + column_offset + 1);
-                column_offset = column_offset + column + 1;
-              }
-
-            }
-            
-            if(column_offset > 0) { out = out + hr + ("Offending line: ").green.bold  + lines[i] + linefeed + linefeed; }
-          }
-          all_out.push(out);*/
+          all_out.push(defaultFindOffenses(src[j]));
         }
         outputLog (paths, f, all_out);
       });
